03461bbff91d028e9db40439d527b94f04c1ce66
Tamas Juhasz <tamasjuhasz84@yahoo.com>
2020-06-12 20:06:14 +0200

    mess

--- nyelvvizsga.py
+++ nyelvvizsga.py
@@ -6,36 +6,43 @@ conn = sqlite3.Connection('nyelvvizsga.db')
 c = conn.cursor()
 c.execute('''
          CREATE TABLE IF NOT EXISTS sikeres (
-                                       nyelv  TEXT
-                                       2009   INTEGER
-                                       2010   INTEGER
-                                       2011   INTEGER
-                                       2012   INTEGER
-                                       2013   INTEGER
-                                       2014   INTEGER
-                                       2015   INTEGER
-                                       2016   INTEGER
-                                       2017   INTEGER
-                                       2018   INTEGER
+                                       nyelv  TEXT,
+                                       '2009'   INTEGER,
+                                       '2010'   INTEGER,
+                                       '2011'   INTEGER,
+                                       '2012'   INTEGER,
+                                       '2013'   INTEGER,
+                                       '2014'   INTEGER,
+                                       '2015'   INTEGER,
+                                       '2016'   INTEGER,
+                                       '2017'   INTEGER,
+                                       '2018'   INTEGER
                                        )
          ''')
+
 conn.commit()
 
 c.execute('''
          CREATE TABLE IF NOT EXISTS sikertelen (
-                                       nyelv  TEXT
-                                       2009   INTEGER
-                                       2010   INTEGER
-                                       2011   INTEGER
-                                       2012   INTEGER
-                                       2013   INTEGER
-                                       2014   INTEGER
-                                       2015   INTEGER
-                                       2016   INTEGER
-                                       2017   INTEGER
-                                       2018   INTEGER
+                                       nyelv  TEXT,
+                                       '2009'   INTEGER,
+                                       '2010'   INTEGER,
+                                       '2011'   INTEGER,
+                                       '2012'   INTEGER,
+                                       '2013'   INTEGER,
+                                       '2014'   INTEGER,
+                                       '2015'   INTEGER,
+                                       '2016'   INTEGER,
+                                       '2017'   INTEGER,
+                                       '2018'   INTEGER
                                        )
          ''')
 conn.commit()
 
-c.execute
+with open('sikeres.csv', 'r', encoding='LATIN2') as sikeres:
+    fejlec=sikeres.readline()
+    for sor in sikeres:
+        s=sor.strip().split(';')
+        c.execute("INSERT INTO sikeres VALUES(?,?,?,?,?,?,?,?,?,?,?)", s)
+    conn.commit()
+    
